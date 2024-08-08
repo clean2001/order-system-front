5171bbe58bccf1bdd20212fde5661fdf44ec1772
Sejeong Kim <clearrworld@gmail.com>
2024-08-08 09:39:29 +0900

    feat: MyPage.vue 완성

--- src/views/MyPage.vue
+++ src/views/MyPage.vue
@@ -6,6 +6,18 @@
                     <v-card-title class="text-center text-h5">
                         회원 정보
                     </v-card-title>
+                    <v-card-text>
+                        <v-table>
+                            <tbody>
+                                <tr v-for="element in memberInfoList" :key="element.key">
+                                    <!-- key-value 형식으로 -->
+                                    <td>{{element.key}}</td>
+                                    <td>{{element.value}}</td>
+                                </tr>
+                            </tbody>
+                        </v-table>
+
+                    </v-card-text>
                     
                 </v-card>
 
@@ -15,6 +27,7 @@
     <OrderListComponent />
 </template>
 <script>
+import axios from 'axios';
 import OrderListComponent from '@/components/OrderListComponent.vue';
 
 export default {
@@ -23,11 +36,23 @@ export default {
     },
     data() {
         return {
-
+            memberInfo: {},
+            memberInfoList: []
         }
     },
     methods: {
 
+    },
+    async created() {
+        const response = await axios.get(`${process.env.VUE_APP_API_BASE_URL}/member/myinfo`);
+        this.memberInfo = response.data.result;
+        this.memberInfoList = [
+            {key: "이름", value: this.memberInfo.name},
+            {key: "email", value: this.memberInfo.email},
+            {key: "도시", value: this.memberInfo.city},
+            {key:"상세주소", value: this.memberInfo.street},
+            {key: "우편번호", value: this.memberInfo.zipcode}
+        ]
     }
 }
 </script>
